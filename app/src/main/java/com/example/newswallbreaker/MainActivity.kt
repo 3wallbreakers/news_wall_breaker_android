842775ab7f4dab48e1fec82b2e5c96b3f8fba4e4
3wallbreakers <3wallbreakers@gmail.com>
2022-05-30 20:23:42 -0700

    Opening the page direct in the app instead of redirecting to a browser

--- app/src/main/java/com/example/newswallbreaker/MainActivity.kt
+++ app/src/main/java/com/example/newswallbreaker/MainActivity.kt
@@ -1,51 +1,24 @@
 package com.example.newswallbreaker
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+
+        val unencodedHtml = """<html><body>Open a page with paywall in a browser > Select ... > Share... > Choose NewsWallBreaker </body></html>""";
+        val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
+        myWebView.loadData(encodedHtml, "text/html", "base64")
     }
 
-    override fun onResume() {
-        super.onResume()
-        handleIntent()
-    }
-
-
-    fun handleIntent() {
-
-        when {
-            intent?.action == Intent.ACTION_SEND -> {
-                intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-
-                    val toast = Toast.makeText(applicationContext, it, Toast.LENGTH_SHORT)
-                    toast.show()
-
-                    val urlString = "https://archive.ph/submit/?url=" + it
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    intent.setPackage("com.android.chrome")
-                    try {
-                        startActivity(intent)
-                    } catch (ex: ActivityNotFoundException) {
-                        // Chrome browser presumably not installed so allow user to choose instead
-                        intent.setPackage(null)
-                        startActivity(intent)
-                    }
-                }
-
-            }
-            else -> {
-                // Handle other intents, such as being started from the home screen
-            }
-        }
-
-    }
 }
