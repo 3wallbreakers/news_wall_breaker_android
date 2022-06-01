dbf3d781c2065dcddc5de5d51eebed6eec1d0033
3wallbreakers <3wallbreakers@gmail.com>
2022-05-31 17:05:29 -0700

    adding a new app launcher

--- app/src/main/java/com/example/newswallbreaker/BrowserActivity.kt
+++ app/src/main/java/com/example/newswallbreaker/BrowserActivity.kt
@@ -2,12 +2,9 @@ package com.example.newswallbreaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Base64
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 
 class BrowserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +28,6 @@ class BrowserActivity : AppCompatActivity() {
     private fun handleShareIntent() {
         if (intent?.action == Intent.ACTION_SEND) {
             intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-                val toast = Toast.makeText(applicationContext, "v3: " + it, Toast.LENGTH_SHORT)
-                toast.show()
-
                 val urlString = "https://3wallbreakers.deta.dev/news_wall_breaker/?url=" + it
                 val myWebView: WebView = findViewById(R.id.webview)
                 myWebView.loadUrl(urlString)
