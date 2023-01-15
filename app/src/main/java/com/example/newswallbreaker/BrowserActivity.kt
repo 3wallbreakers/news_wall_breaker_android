53b7bd457c31c244fe4ea4d90fe47827d84b00b8
3wallbreakers <3wallbreakers@gmail.com>
2023-01-15 12:35:10 -0800

    allow darkmode

--- app/src/main/java/com/example/newswallbreaker/BrowserActivity.kt
+++ app/src/main/java/com/example/newswallbreaker/BrowserActivity.kt
@@ -1,10 +1,15 @@
 package com.example.newswallbreaker
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.webkit.WebSettings.FORCE_DARK_OFF
+import android.webkit.WebSettings.FORCE_DARK_ON
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
 
 class BrowserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +22,20 @@ class BrowserActivity : AppCompatActivity() {
                 return false
             }
         }
-        myWebView.getSettings().setBuiltInZoomControls(true);
+        val mSettings = myWebView.getSettings()
+        mSettings.setBuiltInZoomControls(true);
+
+        //allow dark mode
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    WebSettingsCompat.setForceDark(myWebView.settings, WebSettingsCompat.FORCE_DARK_ON)
+                }
+                Configuration.UI_MODE_NIGHT_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                    WebSettingsCompat.setForceDark(myWebView.settings, WebSettingsCompat.FORCE_DARK_OFF)
+                }
+            }
+        }
     }
 
     override fun onResume() {
